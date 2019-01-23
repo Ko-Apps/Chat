a9177a9b1d95a229066f9d17e536794bd67b96f9
Voltis <v.agolli@live.com>
2019-01-23 16:43:57 +0100

    create message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 // Import GraphQL helpers
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
 // App component styles
@@ -20,6 +20,17 @@ class App extends Component {
     const from = window.prompt('username');
     from && this.setState({ from });
   }
+
+  _createChat = async e => {
+    if (e.key === 'Enter') {
+      const { content, from } = this.state;
+      await this.props.createChatMutation({
+        variables: { content, from }
+      });
+      this.setState({ content: '' });
+    }
+  };
+
   render() {
     const allChats = this.props.allChatsQuery.allChats || [];
     return (
@@ -29,6 +40,15 @@ class App extends Component {
           {allChats.map(message => (
             <Chatbox key={message.id} message={message} />
           ))}
+
+          {/* Message content input */}
+          <input
+            value={this.state.content}
+            onChange={e => this.setState({ content: e.target.value })}
+            type="text"
+            placeholder="Start typing"
+            onKeyPress={this._createChat}
+          />
         </div>
       </div>
     );
@@ -47,4 +67,19 @@ const ALL_CHATS_QUERY = gql`
   }
 `;
 
-export default graphql(ALL_CHATS_QUERY, { name: 'allChatsQuery' })(App);
+const CREATE_CHAT_MUTATION = gql`
+mutation CreateChatMutation($content: String!, $from: String!) {
+  createChat(content: $content, from: $from) {
+    id
+    createdAt
+    from
+    content
+  }
+}
+`;
+
+
+export default compose(
+  graphql(ALL_CHATS_QUERY, { name: 'allChatsQuery' }),
+  graphql(CREATE_CHAT_MUTATION, { name: 'createChatMutation' })
+)(App);
